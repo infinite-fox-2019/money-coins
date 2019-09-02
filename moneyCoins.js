36658d9199fc2deffa2d35c13387addc66babd5a
Sigit Ari Prasetyo <sigitari111@gmial.com>
2019-09-02 20:39:51 +0700

    selesai

--- moneyCoins.js
+++ moneyCoins.js
@@ -1,5 +1,16 @@
 function moneyCoins (money) {
   // your implementation code here
+  var hasil = []
+  var pecahan = [10000, 5000, 2000, 1000, 500, 200, 100, 50, 20, 10, 1]
+
+  for(var i = 0; i < pecahan.length; i++){
+    if(money - pecahan[i] >= 0){
+      hasil.push(pecahan[i])
+      money -= pecahan[i]
+      i = 0
+    }
+  }
+  return hasil
 }
 
 // Drive code
@@ -10,4 +21,4 @@ console.log(moneyCoins(7752))
 //output : [ 5000, 2000, 500, 200, 50, 1, 1 ]
 
 console.log(moneyCoins(37454))
-//output : [ 10000, 10000, 10000, 5000, 2000, 200, 200, 50, 1, 1, 1, 1 ]
+output : [ 10000, 10000, 10000, 5000, 2000, 200, 200, 50, 1, 1, 1, 1 ]
